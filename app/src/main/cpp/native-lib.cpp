c9afcf8ae6433913f535c3c9403479338f213e37
Henry Thasler <git@thasler.org>
2025-04-14 21:43:09 +0200

    link against verovio

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,16 @@
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+
+#include "toolkit.h"
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_henrythasler_sheetmusic_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    vrv::Toolkit tk(false);
+    __android_log_print(ANDROID_LOG_INFO, "Verovio", "%s", "test");
+    std::string hello = "Hello from Verovio " + tk.GetVersion();
+//    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
