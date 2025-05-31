b022e20bb8690fe2177e8602f733ff43404cdd36
Henry <git@thasler.org>
2025-05-31 09:25:15 +0200

    new github workflow

--- app/src/main/java/com/henrythasler/sheetmusic/NotationScreen.kt
+++ app/src/main/java/com/henrythasler/sheetmusic/NotationScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -22,19 +23,20 @@ fun NotationScreen(
 ) {
     val context = LocalContext.current
     var svgDocument by remember { mutableStateOf<String?>(null) }
+    var engraveTimeMillis by remember { mutableLongStateOf(0L) }
 
     LaunchedEffect(Unit) {
-        val timeMillis = measureTimeMillis {
+        engraveTimeMillis = measureTimeMillis {
             svgDocument = viewModel.engraveMusicAsset(context, assetPath)
         }
-        Log.i("Verovio", "Engraving '$assetPath' took $timeMillis ms. (${svgDocument?.length} Bytes)")
+        Log.i("Verovio", "Engraving '$assetPath' took $engraveTimeMillis ms. (${svgDocument?.length} Bytes)")
     }
 
     svgDocument?.let { svg ->
         ScalableCachedSvgImage(
             modifier = Modifier
                 .fillMaxSize(),
-            assetName = assetName,
+            title = "$assetName ($engraveTimeMillis ms, ${svg.length/1024} KiB)",
             svgDocument = svg,
             svgConfig = SvgConfig(null, "Edwin-Roman"),
             canvasConfig = CanvasConfig(0.25f, 32f, 100L),
